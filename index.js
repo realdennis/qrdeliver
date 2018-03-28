069d537a3eff81836039de7a1bcb44ef0cb390af
realdennis <evolon688@gmail.com>
2018-03-28 12:06:42 +0800

    add option to change port

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 const qrcode = require('qrcode-terminal');
 const http = require('http');
 const ip = require('ip');
-const fs = require('fs')
+const fs = require('fs');
 
-let port = 9615
+let port = 9615;//default port
+
+if(process.argv[3]=='-p'){
+    port = process.argv[4];
+}
 
 function createServer(){
     http.createServer((req,res) => {
@@ -18,7 +22,9 @@ if(process.argv[2]!=undefined){
     something = 'http://'+ip.address()+':'+port+'/'+encodeURI(process.argv[2]); 
     createServer();
     qrcode.generate(something);
+    console.log(something);
 }else{
     console.log('usage:')
-    console.log('       qrfile [file ..]       make your file be a qrcode(link) ')
+    console.log('       qrfile [file ..]              make your file be a qrcode(link) ')
+    console.log('       qrfile [file ..] -p [port]    change port if collision')
 }
