57d76f3fe1b1aff75e1589611e92faf571bdbe00
realdennis <realdennis777@gmail.com>
2019-08-19 02:03:39 +0800

    test case modified

---
+++ test/parser_test.ts
@@ -0,0 +1,51 @@
+import { expect } from 'chai';
+import parser from '../src/lib/parser';
+
+import * as ip from 'ip';
+let host = `http://${ip.address()}`;
+
+const path = process.cwd();
+
+//parser('filename',port);
+console.log = () => {};
+describe('Parser Test', () => {
+  describe('filename test', () => {
+    beforeEach(() => {
+      process.chdir(path);
+    });
+    it('nested 1', () => {
+      expect(parser('./src/index.ts', 9999)).to.equal(`${host}:9999/index.ts`);
+    });
+    it('nested 2', () => {
+      expect(parser('./src/lib/parser.ts', 9999)).to.equal(
+        `${host}:9999/parser.ts`
+      );
+    });
+  });
+  describe('Only filename', () => {
+    beforeEach(() => {
+      process.chdir(path);
+    });
+    it('relative test', () => {
+      expect(parser('./package.json', 9999)).to.equal(
+        `${host}:9999/package.json`
+      );
+    });
+    it('direct test', () => {
+      expect(parser('package.json', 9999)).to.equal(
+        `${host}:9999/package.json`
+      );
+    });
+  });
+  describe('file dir not exist', () => {
+    beforeEach(() => {
+      process.chdir(path);
+    });
+    it('not exist dir', () => {
+      expect(() => parser('./foo/bar', 9999)).not.to.throw();
+    });
+    it('not exist dir', () => {
+      expect(() => parser('./asdjoasjdoiajsd/bar', 9999)).not.to.throw();
+    });
+  });
+});
