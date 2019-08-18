da2d4a63cf4b06fdae94766ead4945cb5e1454cd
realdennis <realdennis777@gmail.com>
2019-08-19 01:47:14 +0800

    use ts & nodemon

---
+++ src/lib/server.ts
@@ -0,0 +1,13 @@
+import * as fs from 'fs';
+const http = require('http');
+const fileServer = http.createServer((req: any, res: any) => {
+  res.writeHead(200);
+  const url = req.url || '';
+  try {
+    const file = fs.readFileSync(process.cwd() + decodeURI(url));
+    res.end(file);
+  } catch (err) {
+    url.includes('favicon') && console.log('Some error occuring...');
+  }
+});
+export default fileServer;
