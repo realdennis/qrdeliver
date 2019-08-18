6686dbcd6c94ba44f69bf715bf8aba15c632f775
Dennis <realdennis777@gmail.com>
2019-08-19 00:33:31 +0800

    Parcel integrate (#4)
    * Using parcel to build
    
    * change test code

--- test/parser_test.js
+++ test/parser_test.js
@@ -1,79 +1,90 @@
 var expect = require('chai').expect;
-var parser = require('../dist/lib/parser');
+var parser = require('../src/lib/parser');
 
 const ip = require('ip');
-let host = `http://${ip.address()}`; 
+let host = `http://${ip.address()}`;
 
 const path = process.cwd();
 
 //parser('filename',port);
 
-describe('Parser Test',()=>{
-  describe('filename test',()=>{
-    beforeEach(()=>{
-      process.stdout.write = ()=>{};
+describe('Parser Test', () => {
+  describe('filename test', () => {
+    beforeEach(() => {
+      process.stdout.write = () => {};
       process.chdir(path);
     });
-    afterEach(()=>{
+    afterEach(() => {
       delete process.stdout.write;
     });
-    it('src',()=>{
-      expect(parser('./src/index.js',9999)).to.equal(`${host}:9999/index.js`);
+    it('src', () => {
+      expect(parser('./src/index.js', 9999)).to.equal(`${host}:9999/index.js`);
     });
-    it('src',()=>{
-      expect(parser('./src/lib/parser.js',9999)).to.equal(`${host}:9999/parser.js`);
+    it('src', () => {
+      expect(parser('./src/lib/parser.js', 9999)).to.equal(
+        `${host}:9999/parser.js`
+      );
     });
-    it('src',()=>{
-      expect(parser('./src/lib/server.js',9999)).to.equal(`${host}:9999/server.js`);
+    it('src', () => {
+      expect(parser('./src/lib/server.js', 9999)).to.equal(
+        `${host}:9999/server.js`
+      );
     });
-    it('dist',()=>{
-      expect(parser('./dist/index.js',9999)).to.equal(`${host}:9999/index.js`);
+    it('dist', () => {
+      expect(parser('./src/index.js', 9999)).to.equal(`${host}:9999/index.js`);
     });
-    it('dist',()=>{
-      expect(parser('./dist/lib/parser.js',9999)).to.equal(`${host}:9999/parser.js`);
+    it('dist', () => {
+      expect(parser('./src/lib/parser.js', 9999)).to.equal(
+        `${host}:9999/parser.js`
+      );
     });
-    it('dist',()=>{
-      expect(parser('./dist/lib/server.js',9999)).to.equal(`${host}:9999/server.js`);
+    it('dist', () => {
+      expect(parser('./src/lib/server.js', 9999)).to.equal(
+        `${host}:9999/server.js`
+      );
     });
   });
-  describe('Only filename',()=>{
-    beforeEach(()=>{
-      process.stdout.write = ()=>{};
+  describe('Only filename', () => {
+    beforeEach(() => {
+      process.stdout.write = () => {};
       process.chdir(path);
     });
-    afterEach(()=>{
+    afterEach(() => {
       delete process.stdout.write;
     });
-    it('src',()=>{
-      expect(parser('./package.json',9999)).to.equal(`${host}:9999/package.json`);
+    it('src', () => {
+      expect(parser('./package.json', 9999)).to.equal(
+        `${host}:9999/package.json`
+      );
     });
-    it('src',()=>{
-      expect(parser('./README.md',9999)).to.equal(`${host}:9999/README.md`);
+    it('src', () => {
+      expect(parser('./README.md', 9999)).to.equal(`${host}:9999/README.md`);
     });
-    it('src',()=>{
-      expect(parser('package.json',9999)).to.equal(`${host}:9999/package.json`);
+    it('src', () => {
+      expect(parser('package.json', 9999)).to.equal(
+        `${host}:9999/package.json`
+      );
     });
-    it('dist',()=>{
-      expect(parser('README.md',9999)).to.equal(`${host}:9999/README.md`);
+    it('dist', () => {
+      expect(parser('README.md', 9999)).to.equal(`${host}:9999/README.md`);
     });
   });
-  describe('file dir not exist',()=>{
-    beforeEach(()=>{
+  describe('file dir not exist', () => {
+    beforeEach(() => {
       process.chdir(path);
     });
-    afterEach(()=>{
+    afterEach(() => {});
+    it('not exist dir', () => {
+      expect(() => parser('./foo/bar', 9999)).not.to.throw();
     });
-    it('not exist dir',()=>{
-      expect(()=>parser('./foo/bar',9999)).not.to.throw();
+    it('not exist dir', () => {
+      expect(() => parser('./asdjoasjdoiajsd/bar', 9999)).not.to.throw();
     });
-    it('not exist dir',()=>{
-      expect(()=>parser('./asdjoasjdoiajsd/bar',9999)).not.to.throw();
+    it('not exist dir', () => {
+      expect(() => parser('./fozxjcpojzxcio/bar', 9999)).not.to.throw();
     });
-    it('not exist dir',()=>{
-      expect(()=>parser('./fozxjcpojzxcio/bar',9999)).not.to.throw();
+    it('not exist dir', () => {
+      expect(() => parser('./qwjepjpqowe/bar', 9999)).not.to.throw();
     });
-    it('not exist dir',()=>{
-      expect(()=>parser('./qwjepjpqowe/bar',9999)).not.to.throw();
-    });
-  })
-})
+  });
+});
